26831fbf3a1e4dfe724904e3e9e03596cedc890e
keeut <keeut@naver.com>
2024-02-20 17:14:58 +0900

    node docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
-FROM public.ecr.aws/lambda/nodejs:20
+FROM amazon/aws-lambda-nodejs:16
+# Install Chrome to get all of the dependencies installed
+ADD https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm chrome.rpm
+RUN yum install -y ./chrome.rpm
 
 COPY ./package.json  ${LAMBDA_TASK_ROOT}
 
@@ -12,7 +15,9 @@ COPY ./build ${LAMBDA_TASK_ROOT}
 ENV OPENAI_API_KEY=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
 ENV AWS_ACCESS_KEY_ID=AKIAYS2NSEWDMNIVSZ75
 ENV AWS_SECRET_ACCESS_KEY=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
-ENV STAGE=dev
+ENV STAGE=dev 
+
+RUN ls
 # RUN echo $(ls ./lib)
 
 CMD [ "artist/post.handler" ]
--- src/lib/crawling.ts
+++ src/lib/crawling.ts
@@ -1,6 +1,6 @@
-import { load } from 'cheerio';
-import puppeteer from 'puppeteer';
-import { filterConcertInfo } from './openai.module';
+import { load } from "cheerio";
+import puppeteer from "puppeteer";
+import { filterConcertInfo } from "./openai.module";
 // import { OCRhandler } from './naver/OCR.js';
 
 export const getPosting = async (
@@ -8,15 +8,25 @@ export const getPosting = async (
 ): Promise<
   { name?: string; place?: string; date?: string[]; ticketDate?: string; ticketPlace?: string; postingUrl?: string }[]
 > => {
-  console.log('[artistAccount]', artistAccount);
-  const browser = await puppeteer.launch({ headless: true });
+  console.log("[artistAccount]", artistAccount);
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: "/usr/bin/google-chrome",
+    args: [
+      "--disable-extensions",
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+    ],
+  });
   const page = await browser.newPage();
   await page.setUserAgent(
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
   );
 
-  const res = await page.goto(`https://www.instagram.com`, { waitUntil: 'networkidle2' });
-  console.log('res status', res.status());
+  const res = await page.goto(`https://www.instagram.com`, { waitUntil: "networkidle2" });
+  console.log("res status", res.status());
   let content = await page.content();
   //   console.log('content', content);
 
@@ -26,33 +36,33 @@ export const getPosting = async (
   // await page.click('button[type="submit"]');
   // await page.waitForNavigation({ waitUntil: 'networkidle2' });
 
-  await page.goto(`https://www.instagram.com/${artistAccount}`, { waitUntil: 'networkidle2' });
-  await page.waitForSelector('article');
-  console.log('res status', res.status());
+  await page.goto(`https://www.instagram.com/${artistAccount}`, { waitUntil: "networkidle2" });
+  await page.waitForSelector("article");
+  console.log("res status", res.status());
   content = await page.content();
   //   console.log('content', content);
 
   const $ = load(content);
-  const article = $('article');
+  const article = $("article");
   //   console.log('article html', article.html());
-  const postingArray = article.find('div._aagv img');
-  console.log('postingArray.length', postingArray.length);
+  const postingArray = article.find("div._aagv img");
+  console.log("postingArray.length", postingArray.length);
 
   const filteredPostingArray = [];
   const tempFilteredPostingArray = postingArray.map(async function () {
     const posting = {
-      content: $(this).attr('alt'),
-      img: $(this).attr('src'),
+      content: $(this).attr("alt"),
+      img: $(this).attr("src"),
     };
     // 콘서트 관련 포스팅만 필터링
     if (await filterByConcertRelated(posting)) {
       // 관련있는 경우 구체적인 정보 추출
       const concertInfo = await filterConcertInfo(posting.content);
-      console.log('concertInfo', concertInfo);
-      concertInfo['postingUrl'] = `https://www.instagram.com${$(this).parent().parent().parent().attr('href')}`;
+      console.log("concertInfo", concertInfo);
+      concertInfo["postingUrl"] = `https://www.instagram.com${$(this).parent().parent().parent().attr("href")}`;
 
       // todo: 추출 실패시 수동확인 포스팅으로 저장
-      if (!concertInfo.date && !posting['postingUrl']) console.log('수동확인 포스팅 저장 필요');
+      if (!concertInfo.date && !posting["postingUrl"]) console.log("수동확인 포스팅 저장 필요");
 
       if (
         filteredPostingArray.length === 0 ||
@@ -66,8 +76,8 @@ export const getPosting = async (
     }
   });
   await Promise.all(tempFilteredPostingArray);
-  console.log('filteredPostingArray length', filteredPostingArray.length);
-  console.log('filteredPostingArray', filteredPostingArray);
+  console.log("filteredPostingArray length", filteredPostingArray.length);
+  console.log("filteredPostingArray", filteredPostingArray);
 
   await page.close();
   await browser.close();
@@ -75,20 +85,20 @@ export const getPosting = async (
 };
 
 const filterByConcertRelated = async (posting: { content: string; img: string }) => {
-  console.log('posting', posting);
+  console.log("posting", posting);
   if (!posting.content) return false;
-  const concertRelatedKeywords = ['콘서트', '공연', '라이브', '티켓', '예매', 'concert', 'CONCERT', 'ticket', 'TICKET'];
+  const concertRelatedKeywords = ["콘서트", "공연", "라이브", "티켓", "예매", "concert", "CONCERT", "ticket", "TICKET"];
 
   // 포스팅 글 필터링
   const count = concertRelatedKeywords.filter((keyword) => posting.content.includes(keyword)).length;
   let isConcertRelated = count >= 1;
-  console.log('isConcertRelated 1', isConcertRelated);
+  console.log("isConcertRelated 1", isConcertRelated);
   if (!isConcertRelated) return false;
 
   // 일시 : 1월 1일 / 1.1 / 1. 1 / 1/1
   const datePattern = /\b\d{1,2}월\s*\d{1,2}일|\b\d{1,2}[.\/]\s*\d{1,2}\b/g;
   isConcertRelated = datePattern.test(posting.content);
-  console.log('isConcertRelated 2', isConcertRelated);
+  console.log("isConcertRelated 2", isConcertRelated);
   if (!isConcertRelated) {
     return false;
   }
