f6b0079d04655f0080047bdd309d03b44a059d53
grr1203 <gyrms9412@naver.com>
2024-05-02 18:32:27 +0900

    error시 중지되지 않도록 업데이트

--- src/local/concert.ts
+++ src/local/concert.ts
@@ -1,50 +1,49 @@
-import "./set-env";
+import './set-env';
 
-import puppeteer from "puppeteer";
-import { load } from "cheerio";
-import mysqlUtil from "../lib/mysqlUtil";
-import { filterByConcertRelated } from "../lib/crawling";
-import { extractConcertInfo } from "../lib/openai.module";
-import fs from "fs";
-import axios from "axios";
-import { getPresignedPostUrl, s3Url } from "../lib/aws/s3Util";
-import { checktDateFormat, countObjectValue, wait } from "../lib/util";
-
+import puppeteer from 'puppeteer';
+import { load } from 'cheerio';
+import mysqlUtil from '../lib/mysqlUtil';
+import { filterByConcertRelated } from '../lib/crawling';
+import { extractConcertInfo } from '../lib/openai.module';
+import fs from 'fs';
+import axios from 'axios';
+import { getPresignedPostUrl, s3Url } from '../lib/aws/s3Util';
+import { checktDateFormat, countObjectValue, wait } from '../lib/util';
 
 const getConcert = async (artists) => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.setUserAgent(
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'
   );
 
-  const res = await page.goto(`https://www.instagram.com`, { waitUntil: "networkidle2" });
-  console.log("res status", res.status());
+  const res = await page.goto(`https://www.instagram.com`, { waitUntil: 'networkidle2' });
+  console.log('res status', res.status());
   // let content = await page.content();
   //   console.log('content', content);
 
-  const username = "zzipwooung@gmail.com";
-  const password = "zxc123ZXC!@#";
+  const username = 'zzipwooung@gmail.com';
+  const password = 'zxc123ZXC!@#';
 
   // instagram 로그인
   await page.type('input[name="username"]', username);
   await page.type('input[name="password"]', password);
   await page.click('button[type="submit"]');
-  await page.waitForNavigation({ waitUntil: "networkidle2" });
+  await page.waitForNavigation({ waitUntil: 'networkidle2' });
 
   for (const artist of artists) {
     const artistIdx = artist.idx;
     const artistAccount = artist.instagram_account;
-    console.log("[artistAccount]", artistAccount);
+    console.log('[artistAccount]', artistAccount);
 
     // 일정시간 기다림없이 계속요청시 401 에러 발생
     await wait(4000);
 
     try {
-      await page.goto(`https://www.instagram.com/${artistAccount}`, { waitUntil: "networkidle2" });
-      await page.waitForSelector("div._aagv", { timeout: 10000 });
+      await page.goto(`https://www.instagram.com/${artistAccount}`, { waitUntil: 'networkidle2' });
+      await page.waitForSelector('div._aagv', { timeout: 10000 });
     } catch (e) {
-      fs.writeFileSync("./error.log", `[${artistAccount}]\n ${e}`);
+      fs.writeFileSync('./error.log', `[${artistAccount}]\n ${e}`);
       continue;
     }
 
@@ -55,26 +54,26 @@ const getConcert = async (artists) => {
 
     const postingArray = [];
 
-    $("div._aagv img").map((i, el) => {
+    $('div._aagv img').map((i, el) => {
       postingArray.push({
-        content: $(el).attr("alt"),
-        img: $(el).attr("src"),
-        postingUrl: `https://www.instagram.com${$(el).parent().parent().parent().attr("href")}`,
+        content: $(el).attr('alt'),
+        img: $(el).attr('src'),
+        postingUrl: `https://www.instagram.com${$(el).parent().parent().parent().attr('href')}`,
       });
     });
 
     let concertInfoArray = [];
 
     postingLoop: for (const posting of postingArray) {
-      if (posting.content && posting.content.startsWith("Photo shared by")) {
+      if (posting.content && posting.content.startsWith('Photo shared by')) {
         await wait(1000);
-        await page.goto(posting.postingUrl, { waitUntil: "networkidle2" });
-        await page.waitForSelector("div.x4h1yfo", { timeout: 10000 });
+        await page.goto(posting.postingUrl, { waitUntil: 'networkidle2' });
+        await page.waitForSelector('div.x4h1yfo', { timeout: 10000 });
         const content = await page.content();
         const $2 = load(content);
 
-        let text = "";
-        $2("div.x4h1yfo").map((i, el) => {
+        let text = '';
+        $2('div.x4h1yfo').map((i, el) => {
           text += $(el).text();
         });
         posting.content = text;
@@ -83,28 +82,34 @@ const getConcert = async (artists) => {
       // 콘서트 관련 Text있는 포스팅만 필터링 (콘서트 관련 단어 , 일자)
       if (await filterByConcertRelated(posting)) {
         // 관련있는 경우 구체적인 정보 추출
-        const concertInfo = await extractConcertInfo(posting.content);
+        let concertInfo;
+        try {
+          concertInfo = await extractConcertInfo(posting.content);
+        } catch {
+          console.log('extractConcertInfo error');
+          continue;
+        }
 
-        if (concertInfo["date"] == null) continue;
-        for (const date of concertInfo["date"]) {
+        if (concertInfo['date'] == null) continue;
+        for (const date of concertInfo['date']) {
           if (!checktDateFormat(date)) {
-            console.log("날짜 형식이 잘못되었습니다.", date);
+            console.log('날짜 형식이 잘못되었습니다.', date);
             continue postingLoop;
           }
         }
 
-        console.log("concertInfo", concertInfo);
-        concertInfo["postingUrl"] = posting["postingUrl"];
-        concertInfo["postingImg"] = posting["img"];
+        console.log('concertInfo', concertInfo);
+        concertInfo['postingUrl'] = posting['postingUrl'];
+        concertInfo['postingImg'] = posting['img'];
 
         // 동일한 계시물이 있는지 확인하고 만약 이미 있는 경우, 더 정확한 정보가 있는 posting으로 업데이트
         if (concertInfoArray.length !== 0) {
           for (const item of concertInfoArray) {
             if (JSON.stringify(concertInfo.date.sort()) == JSON.stringify(item.date.sort())) {
               // 더 정보가 많다면 업데이트
-              console.log("same date", concertInfo, item);
+              console.log('same date', concertInfo, item);
               console.log(
-                "확인",
+                '확인',
                 countObjectValue(concertInfo, null),
                 countObjectValue(item, null),
                 concertInfoArray.indexOf(item)
@@ -117,33 +122,33 @@ const getConcert = async (artists) => {
             }
           }
         }
-        if (concertInfo["date"] !== null) {
+        if (concertInfo['date'] !== null) {
           concertInfoArray.push(concertInfo);
         }
       }
     }
 
-    console.log("filteredPostingArray length", concertInfoArray.length);
-    console.log("filteredPostingArray", concertInfoArray);
+    console.log('filteredPostingArray length', concertInfoArray.length);
+    console.log('filteredPostingArray', concertInfoArray);
 
     if (concertInfoArray.length > 0) {
       // 콘서트 별
       for (let i = 0; i < concertInfoArray.length; i++) {
         // 콘서트의 일정 별(하루 단위)
 
-        for (let j = 0; j < concertInfoArray[i]["date"].length; j++) {
+        for (let j = 0; j < concertInfoArray[i]['date'].length; j++) {
           // 이미 저장된 콘서트인지 체크 후 저장
-          const concert = await mysqlUtil.getOne("tb_concert", ["idx"], {
+          const concert = await mysqlUtil.getOne('tb_concert', ['idx'], {
             artist_idx: artistIdx,
             date: concertInfoArray[i].date[j],
           });
           if (concert === null) {
             // 이미지 s3 업로드
-            const response = await axios.get(concertInfoArray[i].postingImg, { responseType: "arraybuffer" });
-            const imageBuffer = Buffer.from(response.data, "binary");
-            const postingImageKey = `${artist.atrist_name}/${concertInfoArray[i].date[j].split(" ")[0]}/posting.png`;
+            const response = await axios.get(concertInfoArray[i].postingImg, { responseType: 'arraybuffer' });
+            const imageBuffer = Buffer.from(response.data, 'binary');
+            const postingImageKey = `${artist.atrist_name}/${concertInfoArray[i].date[j].split(' ')[0]}/posting.png`;
             const postingImageUrl = await getPresignedPostUrl(postingImageKey);
-            await axios.put(postingImageUrl, imageBuffer, { headers: { "Content-Type": "image/png" } });
+            await axios.put(postingImageUrl, imageBuffer, { headers: { 'Content-Type': 'image/png' } });
 
             const concert = {
               artist_idx: artistIdx,
@@ -155,7 +160,7 @@ const getConcert = async (artists) => {
               posting_url: concertInfoArray[i].postingUrl,
               posting_img: s3Url + postingImageKey,
             };
-            await mysqlUtil.create("tb_concert", concert);
+            await mysqlUtil.create('tb_concert', concert);
           }
         }
       }
@@ -176,9 +181,9 @@ const getConcert = async (artists) => {
 (async function () {
   // 전체 Artist post 원할시
 
-  let artists = await mysqlUtil.getMany("tb_artist", [], {});
-  artists = artists.slice(14)
-  console.log('starting from', artists[0].artist_name)
+  let artists = await mysqlUtil.getMany('tb_artist', [], {});
+  artists = artists.slice(14);
+  console.log('starting from', artists[0].artist_name);
   await getConcert(artists);
 
   // 특정 Artist post 원할시
--- src/local/popupStore.ts
+++ src/local/popupStore.ts
@@ -1,45 +1,59 @@
-import "./set-env";
-import puppeteer from "puppeteer";
-import { load } from "cheerio";
-import mysqlUtil from "../lib/mysqlUtil";
-import { extractPopupStoreInfo } from "../lib/openai.module";
-import fs from "fs";
-import { wait } from "../lib/util";
+import './set-env';
+import puppeteer from 'puppeteer';
+import { load } from 'cheerio';
+import mysqlUtil from '../lib/mysqlUtil';
+import { extractPopupStoreInfo } from '../lib/openai.module';
+import fs from 'fs';
+import { wait } from '../lib/util';
 
-const popupstore_account_list = [`popupstorego`, "popupmate", "paulseee", "inas.pick", "pops.official_"];
+const popupstore_account_list = [
+  `popupstorego`,
+  'popupmate',
+  'paulseee',
+  'inas.pick',
+  'pops.official_',
+  'daeding.official',
+  'seoulhotple',
+  '_seoulhotplace',
+  'seoul_trends',
+  'lanline.unnie',
+
+  'seoul_life___',
+  'enjou.seoul',
+];
 
 const getPopupStore = async (popup_account_list) => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.setUserAgent(
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'
   );
 
-  const res = await page.goto(`https://www.instagram.com`, { waitUntil: "networkidle2" });
-  console.log("res status", res.status());
+  const res = await page.goto(`https://www.instagram.com`, { waitUntil: 'networkidle2' });
+  console.log('res status', res.status());
   // let content = await page.content();
   //   console.log('content', content);
 
-  const username = "zzipwooung@gmail.com";
-  const password = "zxc123ZXC!@#";
+  const username = 'zzipwooung@gmail.com';
+  const password = 'zxc123ZXC!@#';
 
   // instagram 로그인
   await page.type('input[name="username"]', username);
   await page.type('input[name="password"]', password);
   await page.click('button[type="submit"]');
-  await page.waitForNavigation({ waitUntil: "networkidle2" });
+  await page.waitForNavigation({ waitUntil: 'networkidle2' });
 
   for (const popup_account of popup_account_list) {
-    console.log("[popup_account]", popup_account);
+    console.log('[popup_account]', popup_account);
 
     // 일정시간 기다림없이 계속요청시 401 에러 발생
     await wait(4000);
 
     try {
-      await page.goto(`https://www.instagram.com/${popup_account}`, { waitUntil: "networkidle2" });
-      await page.waitForSelector("div._aagv", { timeout: 10000 });
+      await page.goto(`https://www.instagram.com/${popup_account}`, { waitUntil: 'networkidle2' });
+      await page.waitForSelector('div._aagv', { timeout: 10000 });
     } catch (e) {
-      fs.writeFileSync("./error.log", `[${popup_account}]\n ${e}`);
+      fs.writeFileSync('./error.log', `[${popup_account}]\n ${e}`);
       continue;
     }
 
@@ -48,26 +62,26 @@ const getPopupStore = async (popup_account_list) => {
 
     const postingArray = [];
 
-    $("div._aagv img").map((i, el) => {
+    $('div._aagv img').map((i, el) => {
       postingArray.push({
-        content: $(el).attr("alt"),
-        img: $(el).attr("src"),
-        postingUrl: `https://www.instagram.com${$(el).parent().parent().parent().attr("href")}`,
+        content: $(el).attr('alt'),
+        img: $(el).attr('src'),
+        postingUrl: `https://www.instagram.com${$(el).parent().parent().parent().attr('href')}`,
       });
     });
 
     const popupStoreInfoArray = [];
 
     postingLoop: for (const posting of postingArray) {
-      if (posting.content && posting.content.startsWith("Photo shared by")) {
+      if (posting.content && posting.content.startsWith('Photo shared by')) {
         await wait(1000);
-        await page.goto(posting.postingUrl, { waitUntil: "networkidle2" });
-        await page.waitForSelector("div.x4h1yfo", { timeout: 10000 });
+        await page.goto(posting.postingUrl, { waitUntil: 'networkidle2' });
+        await page.waitForSelector('div.x4h1yfo', { timeout: 10000 });
         const content = await page.content();
         const $2 = load(content);
 
-        let text = "";
-        $2("div.x4h1yfo").map((i, el) => {
+        let text = '';
+        $2('div.x4h1yfo').map((i, el) => {
           text += $(el).text();
         });
         posting.content = text;
@@ -75,20 +89,26 @@ const getPopupStore = async (popup_account_list) => {
 
       // 콘서트 관련 Text있는 포스팅만 필터링 (콘서트 관련 단어 , 일자)
       // 관련있는 경우 구체적인 정보 추출
-      const popupStoreInfo = await extractPopupStoreInfo(posting.content);
+      let popupStoreInfo;
+      try {
+        popupStoreInfo = await extractPopupStoreInfo(posting.content);
+      } catch {
+        console.log('extractPopupStoreInfo error');
+        continue;
+      }
 
-      if (popupStoreInfo["date"] == null) continue;
+      if (popupStoreInfo?.date == null) continue;
 
-      console.log("popupStoreInfo", popupStoreInfo);
-      popupStoreInfo["postingUrl"] = posting["postingUrl"];
+      console.log('popupStoreInfo', popupStoreInfo);
+      popupStoreInfo['postingUrl'] = posting['postingUrl'];
       // concertInfo["postingImg"] = posting["img"];
 
-      if (popupStoreInfo["date"] !== null) {
+      if (popupStoreInfo['date'] !== null) {
         popupStoreInfoArray.push(popupStoreInfo);
       }
     }
 
-    console.log("filteredPostingArray length", popupStoreInfoArray.length);
+    console.log('filteredPostingArray length', popupStoreInfoArray.length);
 
     for (let i = 0; i < popupStoreInfoArray.length; i++) {
       const popupStoreInfo = popupStoreInfoArray[i];
@@ -100,7 +120,10 @@ const getPopupStore = async (popup_account_list) => {
           time: popupStoreInfo.time,
           posting_url: popupStoreInfo.postingUrl,
         };
-        await mysqlUtil.create("tb_popup_store", popupStore);
+        // 중복 체크
+        const existPopupStore = await mysqlUtil.getOne('tb_popup_store', [], { posting_url: popupStore.posting_url });
+        if (existPopupStore) continue;
+        else await mysqlUtil.create('tb_popup_store', popupStore);
       }
     }
   }
